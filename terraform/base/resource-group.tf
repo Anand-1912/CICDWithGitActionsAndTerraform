8fd3f16f11178f3d72b0a46a5fcfcbcde3268489
Anand-1912 <anandchandrasekaran1912@gmail.com>
2023-08-29 00:06:06 +0100

    Add base terraform

--- terraform/base/resource-group.tf
+++ terraform/base/resource-group.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "res_group"  {
+resource "azurerm_resource_group" "rg"  {
   name = local.resource_group_name
   location = local.location
 }
--- terraform/base/variables.tf
+++ terraform/base/variables.tf
@@ -1,41 +1,41 @@
-variable "location" {
-  default     = "uk south"
-  description = "Azure region where all the resources will be deployed to"
-}
+# variable "location" {
+#   default     = "uk south"
+#   description = "Azure region where all the resources will be deployed to"
+# }
 
-variable "organization" {
-  default     = "jcapcloud"
-  description = "Organization name"
-}
+# variable "organization" {
+#   default     = "jcapcloud"
+#   description = "Organization name"
+# }
 
-variable "location_suffix" {
-  default = "uksouth"
-}
+# variable "location_suffix" {
+#   default = "uksouth"
+# }
 
-variable "resource_group" {
-  default = "rg-superapp-"
-}
+# variable "resource_group" {
+#   default = "rg-superapp-"
+# }
 
-variable "sa_name" {
-  default = "superapptfstorage"
-}
+# variable "sa_name" {
+#   default = "superapptfstorage"
+# }
+
+# variable "tags" {
+#   type        = map(string)
+  
+#   default = {
+#     Department = "JCAP Cloud"
+#     CreatedBy   = "IT Innovation"
+#     Domain        = "Learning"
+#     Environment = "Development"
+#   }
+#   description = <<-HEREDOC
+#   Everything should be tagged, for guidance see:-
+#   https://docs.microsoft.com/en-us/azure/azure-resource-manager/resource-manager-subscription-governance#resource-tags
+# HEREDOC
+# }
 
 variable "environment" {
   default     = "dev"
   description = "Name of the target environment"
-}
-
-variable "tags" {
-  type        = map(string)
-  
-  default = {
-    Department = "JCAP Cloud"
-    CreatedBy   = "IT Innovation"
-    Domain        = "Learning"
-    Environment = "Development"
-  }
-  description = <<-HEREDOC
-  Everything should be tagged, for guidance see:-
-  https://docs.microsoft.com/en-us/azure/azure-resource-manager/resource-manager-subscription-governance#resource-tags
-HEREDOC
 }
