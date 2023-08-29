11dcd86afa83c0038d2b9cf9ee3eead704136f77
Anand-1912 <anandchandrasekaran1912@gmail.com>
2023-08-29 08:50:21 +0100

    update tf storage account name template

--- terraform/base/locals.tf
+++ terraform/base/locals.tf
@@ -1,11 +1,11 @@
-resource "random_id" "random" {
-  byte_length = 8
-}
+# resource "random_id" "random" {
+#   byte_length = 8
+# }
 
 locals {
   name_suffix                              = "001"
   resource_group_name                      = "rg-superapp-${var.environment}-westeurope-${local.name_suffix}"
-  storage_account_name                     = "sttfstatefile${var.environment}${local.name_suffix}"
+  storage_account_name                     = "stsuperapptfstatefile${var.environment}${local.name_suffix}"
   location                                 = "north europe"
   location_suffix                          = "northeurope"
 }
